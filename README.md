6a36ba1100ce160b9b7af016ff9df848ec862fb4
AkshayGondaliya <axgondaliya@gmail.com>
2022-10-14 13:38:49 +0530

    Change README.md

--- README.md
+++ README.md
@@ -15,3 +15,14 @@ Requirements
 
 `sh requirements.sh`
 
+After successful execution of this requirement.sh file you will land in docker container(name of the container = wobottask2).
+
+Now, Go inside datadrive folder using command ..
+
+`cd datadrive`
+
+Now, install other Requirements using command ..
+
+`sh requirement2.sh`
+
+
---
+++ requirement2.sh
@@ -0,0 +1,19 @@
+apt-get update
+apt-get upgrade -y
+apt-get install git -y
+apt install software-properties-common -y
+apt update
+add-apt-repository ppa:deadsnakes/ppa
+apt install python3.8 -y
+python3.8 --version
+apt-get install wget
+wget --version
+apt update
+apt install python3-opencv -y
+python3 -c "import cv2; print(cv2.__version__)"
+apt update
+apt install python3-pip -y
+pip3 --version
+pip3 install --upgrade pip
+apt update apt install nano
+pip3 install --upgrade opencv-python==4.2.0.34
